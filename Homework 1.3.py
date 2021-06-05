bfecb1492f6aacc276ef8ad48d7e5ef75b105b8b
458Shcheglov <85124802+458Shcheglov@users.noreply.github.com>
2021-06-05 15:55:26 +0300

    Add files via upload

---
+++ Homework 1.3.py
@@ -0,0 +1,12 @@
+# n = int(input('Введите число - '))
+# n = str(n)
+# nn = n + n
+# nnn = n + n + n
+#
+# n = int(n)
+# nn = int(nn)
+# nnn = int(nnn)
+# print(n + nn + nnn)
+
+n = input('Введите положительное число - ')
+print(f'{n} + {n + n} + {n + n + n} = {int(n) + int(n + n) + int(n + n + n)}')
